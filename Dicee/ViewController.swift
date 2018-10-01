26d1c457470902bd4fac09421c03a0e4ec534395
Milica Sucevic <milicasucevic@gmail.com>
2018-10-01 19:50:16 +0100

    Clean Code - DRY Principle

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImages()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,14 +34,16 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        updateDiceImages()
+        
+    }
+    
+    func updateDiceImages(){
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
-        print(randomDiceIndex1)
-        
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-        
     }
     
 }
